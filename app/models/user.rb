25cf21733e26167ee34662a7341c164a30063fdd
grandtheft-spaceship <justdizon71@gmail.com>
2017-05-30 17:38:04 -0700

    :art: Creates methods and helpers to send messages in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,28 @@ class User < ApplicationRecord
   validates :phone_number, presence: true, uniqueness: true
   validates_length_of :password, in: 6..20, on: :create
 
+  def send_message_via_sms(message)
+    @app_number = ENV["TWILIO_NUMBER"]
+    @client = Twilio::REST::Client.new ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"]
+    sms_message = @client.account.messages.create(
+    from: @app_number,
+    to: self.phone_number,
+    body: message
+    )
+  end
+
+  def check_for_reservations_pending
+    if pending_reservation
+      pending_reservation.notify_host(true)
+    end
+  end
+
+  def pending_reservation
+    self.reservations.where(status: "pending").first
+  end
+
+  def pending_reservations
+    self.reservations.where(status: "pending")
+  end
+
 end
